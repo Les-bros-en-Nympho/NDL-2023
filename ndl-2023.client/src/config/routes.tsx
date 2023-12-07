3460ddb649749cfcbf8747f1300f7733a6512b0f
Furiza31 <hugo.wendjaneh@gmail.com>
2023-12-07 20:31:57 +0100

    fix(Routes): add type

--- ndl-2023.client/src/config/routes.tsx
+++ ndl-2023.client/src/config/routes.tsx
@@ -1,5 +1,6 @@
 import { Root } from '../layouts/Root';
 import { Home } from '../pages/Home';
+import { RouteObject } from 'react-router-dom';
 
 export const routes = [
     {
@@ -32,4 +33,4 @@ export const routes = [
             }
         ]
     },
-];
+] as RouteObject;

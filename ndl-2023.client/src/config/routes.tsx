11b13edebcf40e3aaa3e6ccd5020e1588daa2bbe
Furiza31 <hugo.wendjaneh@gmail.com>
2023-12-07 23:01:19 +0100

    feat(i18n): set root for languages

--- ndl-2023.client/src/config/routes.tsx
+++ ndl-2023.client/src/config/routes.tsx
@@ -4,37 +4,42 @@ import { Navigate, RouteObject } from 'react-router-dom';
 
 export const routes = [
     {
-        path: '/:locale',
-        element: <Root />,
+        path: '/',
         children: [
             {
-                path: '', // Le chemin vide ici correspond à '/:locale', donc cela pointe vers '/:locale'
-                element: <Home />
+                path: '/:locale',
+                element: <Root />,
+                children: [
+                    {
+                        path: '',
+                        element: <Home />
+                    },
+                    {
+                        path: 'training',
+                        element: <Home />
+                    },
+                    {
+                        path: 'account',
+                        element: <Home />
+                    },
+                    {
+                        path: 'help',
+                        element: <Home />
+                    },
+                    {
+                        path: 'privacy',
+                        element: <Home />
+                    },
+                    {
+                        path: 'login',
+                        element: <Home />
+                    }
+                ]
             },
             {
-                path: 'training', // Chemin relatif par rapport à '/:locale'
-                element: "" // Assurez-vous de remplir l'élément correspondant
-            },
-            {
-                path: 'account', // Chemin relatif par rapport à '/:locale'
-                element: "" // Assurez-vous de remplir l'élément correspondant
-            },
-            {
-                path: 'help', // Chemin relatif par rapport à '/:locale'
-                element: "" // Assurez-vous de remplir l'élément correspondant
-            },
-            {
-                path: 'privacy', // Chemin relatif par rapport à '/:locale'
-                element: "" // Assurez-vous de remplir l'élément correspondant
-            },
-            {
-                path: 'login', // Chemin relatif par rapport à '/:locale'
-                element: "" // Assurez-vous de remplir l'élément correspondant
+                path: '/',
+                element: <Navigate to="/en" />
             }
         ]
-    },
-    {
-        path: '*', // Route par défaut pour les chemins inconnus
-        element: <Navigate to="/404" />
     }
 ] as RouteObject[];

582aede050959ce08e9b8519b1cd8761015c57c8
Furiza31 <hugo.wendjaneh@gmail.com>
2023-12-08 00:03:13 +0100

    feat(scss): colors

--- ndl-2023.client/src/components/header/Header.tsx
+++ ndl-2023.client/src/components/header/Header.tsx
@@ -1,6 +1,22 @@
+import i18n from '../../i18n';
+import '../../styles/header/header.scss';
+
 export const Header = () => {
+    const { t } = i18n
+    
     return (
         <header>
+            <div className="title">
+                <img src="/images/logo.png" alt="Logo" />
+                <h1>{ t('title') }</h1>
+            </div>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Quizz</a>
+                <a href="">News</a>
+                <a href="">Leader board</a>
+                <a href="">Login</a>
+            </nav>
         </header>
     );
 }
--- ndl-2023.client/src/pages/Home.tsx
+++ ndl-2023.client/src/pages/Home.tsx
@@ -1,7 +1,5 @@
-import i18n from '../i18n'
-
 export const Home = () => {
     return (
-        <h1>{ i18n.t('title') }</h1>
+        <></>
     )
 }

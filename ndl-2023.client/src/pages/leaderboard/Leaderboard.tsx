61a49b493cf555249d180d34f3b838d88e89086d
Sonixray <loangayrard@gmail.com>
2023-12-08 04:39:21 +0100

    FRONT: Leaderboard page in progress...

--- ndl-2023.client/src/pages/leaderboard/Leaderboard.tsx
+++ ndl-2023.client/src/pages/leaderboard/Leaderboard.tsx
@@ -1,7 +1,50 @@
+// Leaderboard.jsx
+
+import '../../styles/leaderboard/leaderboard.scss';
+import { Link } from 'react-router-dom';
+
 export const Leaderboard = () => {
     return (
-        <section id="leaderboard">
-            <h1>Leaderboard</h1>
-        </section>
+        <div className="leaderboard">
+        <div className="leaderboard-header">Leaderboard</div>
+        <ul className="leaderboard-list">
+          <li className="leaderboard-item">
+            <span className="medal">
+              <Link to="">
+                <img src="/images/gold-medal.png" alt="Gold Medal" />
+              </Link>
+            </span>
+            <span>1</span>
+            <span>Player1</span>
+            <span>1000</span>
+          </li>
+          <li className="leaderboard-item">
+            <span className="medal">
+              <img src="silver-medal.png" alt="Silver Medal" />
+            </span>
+            <span>2</span>
+            <span>Player2</span>
+            <span>950</span>
+          </li>
+          <li className="leaderboard-item">
+            <span className="medal">
+              <img src="bronze-medal.png" alt="Bronze Medal" />
+            </span>
+            <span>3</span>
+            <span>Player3</span>
+            <span>900</span>
+          </li>
+          <li className="leaderboard-item">
+            <span>4</span>
+            <span>Player4</span>
+            <span>850</span>
+          </li>
+          <li className="leaderboard-item">
+            <span>5</span>
+            <span>Player5</span>
+            <span>800</span>
+          </li>
+        </ul>
+      </div>
     )
-}
+};

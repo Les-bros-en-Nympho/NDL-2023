77b63c478dc4c18ee474f7cad10e4bd1f002673e
Sonixray <loangayrard@gmail.com>
2023-12-08 05:39:50 +0100

    FRONT: Leaderboard style wip...

--- ndl-2023.client/src/pages/leaderboard/Leaderboard.tsx
+++ ndl-2023.client/src/pages/leaderboard/Leaderboard.tsx
@@ -11,7 +11,7 @@ export const Leaderboard = () => {
           <li className="leaderboard-item">
             <span className="medal">
               <Link to="">
-                <img src="/images/gold-medal.png" alt="Gold Medal" />
+                <img src="/images/gold_medal.png" alt="Gold Medal" />
               </Link>
             </span>
             <span>1</span>
@@ -20,7 +20,9 @@ export const Leaderboard = () => {
           </li>
           <li className="leaderboard-item">
             <span className="medal">
-              <img src="silver-medal.png" alt="Silver Medal" />
+              <Link to="">
+                <img src="/images/silver_medal.png" alt="Silver Medal" />
+              </Link>
             </span>
             <span>2</span>
             <span>Player2</span>
@@ -28,18 +30,30 @@ export const Leaderboard = () => {
           </li>
           <li className="leaderboard-item">
             <span className="medal">
-              <img src="bronze-medal.png" alt="Bronze Medal" />
+              <Link to="">
+                <img src="/images/bronze_medal.png" alt="Bronze Medal" />
+              </Link>
             </span>
             <span>3</span>
             <span>Player3</span>
             <span>900</span>
           </li>
           <li className="leaderboard-item">
+            <span className="medal">
+              <Link to="">
+                <img src="/images/honor_medal.png" alt="Honor Medal" />
+              </Link>
+            </span>
             <span>4</span>
             <span>Player4</span>
             <span>850</span>
           </li>
           <li className="leaderboard-item">
+          <span className="medal">
+              <Link to="">
+                <img src="/images/honor_medal.png" alt="Honor Medal" />
+              </Link>
+            </span>
             <span>5</span>
             <span>Player5</span>
             <span>800</span>

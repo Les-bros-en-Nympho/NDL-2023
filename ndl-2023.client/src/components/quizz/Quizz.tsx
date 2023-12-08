3a35060db47d6d586f9d02cc5008a0eb5539f252
Sonixray <loangayrard@gmail.com>
2023-12-08 07:48:09 +0100

    Quizz over

--- ndl-2023.client/src/components/quizz/Quizz.tsx
+++ ndl-2023.client/src/components/quizz/Quizz.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 export const QuizzComponent = () => {
 
     const [statement, setStatement] = useState("");
+    const [response, setResponse] = useState("");
 
     function ReloadRandom(){
         API.getInstance().get("/TrueOrFalse/random").then((response) => {
@@ -23,6 +24,8 @@ export const QuizzComponent = () => {
         let json = null;
         API.getInstance().get(`/TrueOrFalse/guess/${responseP}`).then((response) => {
             console.log(response);
+            if(response != null)
+                setResponse(response["explaination"]);
            json = response;
         });
         ReloadRandom();
@@ -40,16 +43,25 @@ export const QuizzComponent = () => {
     return (
         <>
             <div className="box">
-                <div className="button button-false" onClick={GuessAsFalse}>
-                    <h1>FAUX</h1>
+                <div className="yesno">
+                    <div className="button button-false" onClick={GuessAsFalse}>
+                        <h1>FAUX</h1>
+                    </div>
+                    <div className="button button-true" onClick={GuessAsTrue}>
+                        <h1>VRAI</h1>
+                    </div>
                 </div>
                 <div className="statement">
                     <p>
-                    {statement}
+                        {statement}
                     </p>
                 </div>
-                <div className="button button-true" onClick={GuessAsTrue}>
-                    <h1>VRAI</h1>
+                <div>
+
+                    <br></br>
+                    <p className="response">
+                        Réponse précédente : {response}
+                    </p>
                 </div>
             </div>
         </>
